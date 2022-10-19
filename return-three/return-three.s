dd0589b5700a05557b98794853e5c08988a9f38b
tsensei <tjsiam019@gmail.com>
2022-10-19 21:15:31 +0600

    Complete Assign.1

---
+++ return-three/return-three.s
@@ -0,0 +1,21 @@
+#PURPOSE:	Returns the number three as status code
+#		  	after program exits, which can be seen using
+#		  	`echo $?`
+
+#VARIABLES:	Following registers are used
+#			%eax - Code for system call
+#			%ebx - Holds return status code
+
+.section .data
+
+.section .text
+
+	.globl _start
+	_start:
+		movl $1, %eax		# Stores 1 in register %eax
+							# which is the system call for exit
+
+		movl $3, %ebx		# Stores 3 in register %ebx
+							# which gets returned as status code
+
+		int $0x80			# interrupts program for syscall
